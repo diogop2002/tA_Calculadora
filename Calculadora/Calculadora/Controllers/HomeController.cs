735258575e4a2f081eb089f210765c77353cebc7
diogop2002 <aluno23030@ipt.pt>
2022-03-22 14:48:14 +0000

    adição do processamento dos algarismos

--- Calculadora/Calculadora/Controllers/HomeController.cs
+++ Calculadora/Calculadora/Controllers/HomeController.cs
@@ -17,12 +17,59 @@ namespace Calculadora.Controllers
         [HttpGet] // este anotador é facultativo
         public IActionResult Index()
         {
+            // preparar os dados a serem enviados para a View na primeira interação
+            ViewBag.Visor = "0";
             return View();
         }
 
         [HttpPost] // só quando o fomulario for submetido em 'post' ele será acionado
-        public IActionResult Index(string botao)
+        public IActionResult Index(string botao, string visor)
         {
+
+            //testar valor do botao
+            switch (botao)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "0":
+                    //pressionei um algarismo
+                    if(visor == "0")
+                    {
+                        visor = botao;
+                    }
+                    else
+                    {
+                        visor = visor + botao;
+                    }
+                    //desafio : fazer em modo algebrico esta operação...
+                    break;
+                
+                case ",":
+                    // foi pressionado ','            
+                    if (!visor.Contains(',')) visor += ',';
+                    break;
+
+                case "+/-":
+                    // vamos inverter o valor do 'visor'
+                    if (visor.StartsWith('-')) visor = visor[1..];
+                    else visor = '-' + visor;
+
+
+                    // sugestão: fazer d forma algebrica
+                    break;
+            }
+
+            // preparar dados para serem enviados à View
+            ViewBag.Visor = visor;
+
+
             return View();
         }
 
